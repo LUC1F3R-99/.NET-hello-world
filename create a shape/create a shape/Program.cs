08eb6fa92767b6390db804cb96b5133ff1e8ddc4
Thushara_thilan <146915452+LUC1F3R-99@users.noreply.github.com>
2024-06-19 11:15:18 +0530

    kj

---
+++ create a shape/create a shape/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string characterName = "John";
+            int characterAge;
+
+            characterAge =  35;
+
+            Console.WriteLine("There once wasf a man named "+ characterName);
+            Console.WriteLine("He was " + characterAge + " yesrs old");
+
+            characterName = "mike";
+
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didnt like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
